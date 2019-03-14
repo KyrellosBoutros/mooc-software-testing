bac04c82a0aa32588ba0ef1f122edf79ffd85e7e
Boutros <kyrellos.boutros.extern@techem.de>
2019-03-14 08:54:54 +0100

    correct class CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,14 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
 
+    @Test
+    public void gHappyFirstTest() {
+        CaesarShiftCipher CSC = new CaesarShiftCipher();
+        String result = CSC.CaesarShiftCipher("atz", 3);
+        Assertions.assertEquals("dwc",result);
+    }
 }
